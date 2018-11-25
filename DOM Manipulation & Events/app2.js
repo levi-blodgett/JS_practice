858cc6ad80c33f6b3667e344e8e9f1094f332f63
Levi <levi_blodgett@outlook.com>
2018-11-25 17:47:37 -0600

    1/2way through event listener lesson

--- DOM Manipulation & Events/app2.js
+++ DOM Manipulation & Events/app2.js
@@ -17,7 +17,7 @@ newHeading.appendChild(document.createTextNode('Task List'));
 // Get the old heading
 const oldHeading = document.getElementById('task-title');
 
-// Parent
+// Parent, for the replaceChild method to be able to grab onto the old heading and replace it
 const cardAction = document.querySelector('.card-action');
 
 // Replace
@@ -37,7 +37,6 @@ lisst.removeChild(liss[3]);
 
 // Classes & attributes
 const firstLi = document.querySelector('li:first-child');
-
 const lenk = firstLi.children[0];
 let vol;
 
@@ -55,7 +54,30 @@ vol = lenk.setAttribute('href', 'http://google.com');
 lenk.setAttribute('title', 'Google');
 vol = lenk.hasAttribute('title');
 lenk.removeAttribute('title');
+lenk.setAttribute('style', 'visibility: hidden;');
 vol = lenk;
 
 
 console.log(vol);
+
+//////////////////
+// EVENT LISTENERS AND THE EVENT OBJECT
+//////////////////
+
+
+// Grabs the item with the "clear-tasks" class (in this case), and adds an event listener
+// The event listener takes in an action first, and then what it will do when that action happens
+document.querySelector('.clear-tasks').addEventListener('click', function(e){
+  console.log("Hello World");
+
+  e.preventDefault(); // Passing in the e as the event object, and preventing it from doing the default purpose of the button
+})
+
+// Adding a "#" will make it so the page doesn't refresh upon clicking the link button
+
+// This is the preferred way, as it is cleaner always to have a named function to put in
+document.querySelector('.clear-tasks2').addEventListener('click', onClick);
+
+function onClick(e){
+  console.log('Clicked');
+}
