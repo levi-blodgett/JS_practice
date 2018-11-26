e5369318de84019cd5c1e137af2dee9249c9c942
Levi <levi_blodgett@outlook.com>
2018-11-25 18:26:59 -0600

    Done with the event listener lesson

--- DOM Manipulation & Events/app2.js
+++ DOM Manipulation & Events/app2.js
@@ -60,6 +60,7 @@ vol = lenk;
 
 console.log(vol);
 
+
 //////////////////
 // EVENT LISTENERS AND THE EVENT OBJECT
 //////////////////
@@ -75,9 +76,41 @@ document.querySelector('.clear-tasks').addEventListener('click', function(e){
 
 // Adding a "#" will make it so the page doesn't refresh upon clicking the link button
 
-// This is the preferred way, as it is cleaner always to have a named function to put in to it
+// This is the preferred way, as it is cleaner always to have a named function to put in
 document.querySelector('.clear-tasks2').addEventListener('click', onClick);
 
 function onClick(e){
-  console.log('Clicked');
+  // console.log('Clicked');
+  let vil;
+
+  vil = e;
+
+  // Event target element
+  vil = e.target;
+  vil = e.target.id;
+  vil = e.target.className;
+  vil = e.target.classList; // If you want the same thing as above but as a collection
+
+  // Event type
+  vil = e.type;
+
+  // Timestamp
+  vil = e.timeStamp;
+
+  // Coords event relative to the window
+  vil = e.clientY; // Returns the number of pixels from the top of the window they clicked it from
+  vil = e.clientX; // Returns the number of pixels from the left of the window they clicked it from
+
+  // Coords event relative to the element
+  vil = e.offsetY; // Returns the number of pixels from the top of the element they clicked it from
+  vil = e.offsetX; // Returns the number of pixels from the left of the element they clicked it from
+
+  console.log(vil);
 }
+
+
+//////////////////
+// MOUSE EVENTS
+//////////////////
+
+
